04ecadef0bf66a0e3f5a74a7fa429c1f0a4afd7f
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2026-01-20 04:13:00 +0600

    feat: implement dashboard authentication UI components for login, forget password, OTP verification, and password reset

--- src/components/Dashboard/Auth/ForgetPassword/ForgetPassword.tsx
+++ src/components/Dashboard/Auth/ForgetPassword/ForgetPassword.tsx
@@ -32,7 +32,7 @@ export default function ForgetPassword() {
       <div className="w-full max-w-lg bg-white rounded-2xl shadow-lg px-8 py-10">
         {" "}
         <div className="text-center">
-          <h1 className="text-3xl font-bold text-[#0B3B36]">
+          <h1 className="text-3xl font-bold text-[#07589E]">
             Reset Your Password{" "}
           </h1>
           <p className="text-[#343A40] mt-1 text-sm">
@@ -54,7 +54,7 @@ export default function ForgetPassword() {
           </div>
 
           <Button
-            className="w-full bg-[#0B3B36] hover:bg-[#0B3B36] mt-4 text-white cursor-pointer"
+            className="w-full bg-[#07589E] hover:bg-[#07589E] mt-4 text-white cursor-pointer"
             onClick={handleSendCode}
             disabled={loading}
           >
